06d2a987cb07317f5e6d972dc3e93389fac0e993
halfNeon <92912029+halfNeon@users.noreply.github.com>
2024-01-25 13:07:09 +0530

    Add files via upload

---
+++ numpy.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+'''#0-d array
+arr1=np.array([1])
+print(arr1)
+
+#1-d array
+arr1=np.array([1,2,3])
+print(arr1)
+
+#2-d array
+arr1=np.array([[1,2,3],[4,5,6]])
+print(arr1)
+
+#3-d array
+arr1=np.array([[[1,2],[4,5],[6,7]]])
+print(arr1)
+
+#1-d array slicing 
+arr1=np.array([1,2,3,4,5,6,7,8,9])
+print(arr1[2:7])
+
+#2-d array slicing 
+arr1=np.array([[1,2,3,4],[5,6,7,8]])
+print(arr1[1:3,1])
+
+#shape of a 2d array
+arr1=np.array([[1,2,3,4],[5,6,7,8]])
+shape=np.shape(arr1)
+print(shape)
+
+#reshape an array
+arr1=np.array([1,2,3,4,5,6,7,8])
+shape=arr1.reshape(4,2)
+print(shape)
+
+#iterarting the elements of 2-d array
+arr1=np.array([[1,2,3,4],[5,6,7,8]])
+for x in arr1:
+    for i in x:
+        print (i)
+
+#stack of an array
+arr1= np.array([[1,2],[3,4]])
+arr2= np.array([[5,6],[7,8]])
+stackarray=np.stack((arr1,arr2),dtype=int)
+print(stackarray)
+
+#horizontal stack
+arr1= np.array([[1,2],[3,4]])
+arr2= np.array([[5,6],[7,8]])
+stackarray=np.hstack((arr1,arr2),dtype=int)
+print(stackarray)
+
+#vertical stack
+arr1= np.array([[1,2],[3,4]])
+arr2= np.array([[5,6],[7,8]])
+stackarray=np.vstack((arr1,arr2),dtype=int)
+print(stackarray)'''
+
+
